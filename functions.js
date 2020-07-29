e04796e04728272223ffee4c9f8c2d6b29c26c0b
Stephanie Resis <sresis@umich.edu>
2020-07-28 18:08:34 -0700

    completed part 2

--- functions.js
+++ functions.js
@@ -4,13 +4,42 @@
 // 1. isHometown
 
 // Define your function here
+function isHometown(town) {
+
+	return town === 'San Francisco';
+}
+//console.log(isHometown('San Francisco'));
+//console.log(isHometown('San Jose'));
 
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
 
+	return(`${firstName} ${lastName}`);
+}
+console.log(getFullName('Stephanie', 'Resis'));
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(price, state, tax =0.05) {
+	const subtotal = price * (1 + tax);
+	let fee = 0;
+
+	if (state === 'CA') {
+		fee = (0.03 * subtotal);
+	}
+	else if (state === 'PA') {
+		fee = 2;
+	}
+	else if (state === 'MA') {
+		if (base_price <= 100) {
+			fee = 1;
+		}
+		else {
+			fee = 3;
+		}
+	}
+	return subtotal + fee;
+}
+console.log(calculateTotal(40, 'CA', 0.05));
--- objects.js
+++ objects.js
@@ -3,11 +3,35 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
-}
 
+  const wordCounts = {};
+
+  for (const word of phrase.split(' ')) {
+  	if (wordCounts.hasOwnProperty(word)) {
+  		wordCounts[word] += 1;
+  	}
+  	else {
+  		wordCounts[word] = 1;
+  	}
+  }
+  return wordCounts;
+}
+//console.log(countWords('Hello how are you are you good'));
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+	const melonPrices = {
+		2.50: ['Cantaloupe', 'Honeydew', 'apple'],
+		2.05: ['Watermelon'],
+		3.25: ['Musk', 'Crenshaw'],
+		14.25: ['Christmas']
+	};
+
+	if (!melonPrices.hasOwnProperty(price)) {
+		return;
+	}
+
+	const melonList = melonPrices[price];
+	return melonList.sort(); 
 }
+console.log(getMelonsAtPrice(2.50));
